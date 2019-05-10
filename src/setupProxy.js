f35e231b9b800c8a23e203b1ba948dadad395e19
Ben Soutendijk <devaent1316@gmail.com>
2019-05-10 15:23:18 -0500

    add proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  if (process.env.NODE_ENV === 'production') {
+    app.use(proxy('/api', { target: 'http://bgood.tv:5000/' }));
+  } else {
+    app.use(proxy('/api', { target: 'http://localhost:5000/' }));
+  }
+};
