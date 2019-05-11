1a4e96c98a50439681811e86fe04bcd8540c46b6
Ben Soutendijk <devaent1316@gmail.com>
2019-05-10 19:44:08 -0500

    make changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button><a href='/api/auth/mixer/login'>Login With Mixer</a></Button>
         <iframe className="Channel" title="mixer-channel" src="https://mixer.com/embed/player/bgood"></iframe>
       </div>
     );
