dce4c5236b1851e641152366481b74365ceb67f1
Ben Soutendijk <devaent1316@gmail.com>
2019-05-10 20:23:37 -0500

    update proxy for heroku app

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(proxy('/api', { target: 'http://bgood.tv:5000/' }));
+    app.use(proxy('/api', { target: 'https://bgoodtv.herokuapp.com/' }));
   } else {
     app.use(proxy('/api', { target: 'http://localhost:5000/' }));
   }
