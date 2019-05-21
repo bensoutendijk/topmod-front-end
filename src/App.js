e29dbfdcfec74a567a0b6f0cc739209665f4c2e8
Ben Soutendijk <devaent1316@gmail.com>
2019-05-21 14:05:35 -0500

    add images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 
+
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    }
+  }
+
+  async componentDidMount() {
+    const user = await fetch('/api/auth/mixer/current');
+    const userJson = await user.json();
+    console.log(userJson);
+  }
+
   render() {
     return (
       <div className="App">
