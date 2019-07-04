e0ac20e55a9426ff55032fabecce12b5615ac0be
Ben Soutendijk <devaent1316@gmail.com>
2019-07-04 13:15:53 -0500

    create layout of login page

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,18 +1,133 @@
 import React from 'react';
 
-import { makeStyles, createStyles } from '@material-ui/styles';
+import { ThemeProvider, makeStyles, createStyles } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+  },
+  overrides: {
+    MuiGrid: {
+      item: {
+        width: '100%',
+      },
+    },
+    MuiTextField: {
+      root: {
+        width: '80%',
+        maxWidth: '400px'
+      },
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => createStyles({
-
+  root: {
+    height: '100%',
+    flexGrow: 1,
+  },
+  container: {
+    height: '100%',
+  },
+  imageContainer: {
+    height: '100%',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+  },
+  loginButton: {
+    width: '100%',
+    maxWidth: '400px'
+  },
+  loginContainer: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+  inputContainer: {
+    margin: 'auto',
+    width: '80%',
+    maxWidth: '400px',
+  },
+  checkbox: {
+    display: 'flex',
+    paddingLeft: '10%',
+  },
+  textField: {
+    paddingTop: '12px'
+  },
 }));
 
 function LogIn() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <h1>Log In</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <Grid className={classes.container} container>
+          <Grid className={classes.imageContainer} item xs={6}>
+            <img className={classes.image} src="/assets/images/login.jpeg" />
+          </Grid>
+          <Grid item xs={6}>
+            <Grid className={classes.loginContainer} container direction="column" spacing={4}>
+              <Grid item>
+                <Typography variant="h3">Log In</Typography>
+              </Grid>
+              <Grid item>
+                <Grid className={classes.inputContainer} container direction="column" alignItems="flex-start">
+                  <Grid className={classes.textField} item>
+                  <TextField 
+                    variant="outlined" 
+                    label="Email"
+                    type="email"
+                    name="email"
+                    autoComplete="email"
+                  />
+                  </Grid>
+                  <Grid className={classes.textField} item>
+                    <TextField 
+                      variant="outlined" 
+                      label="Password"
+                      type="password"
+                      name="password"
+                    />
+                  </Grid>
+                  <Grid className={classes.checkbox} item>
+                    <FormControlLabel 
+                      control={
+                        <Checkbox />
+                      }
+                      label="Remember Me"
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Button className={classes.loginButton} variant="contained" color="secondary">Log In</Button>
+              </Grid>
+              <Grid item>
+                <Grid container direction="row" justify="space-between">
+                  <Link>Forgot Password?</Link>
+                  <Link>Don't have an account yet? Sign up!</Link>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
   )
 }
 
