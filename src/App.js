fb4bb50aca7ed6bb2532f2d78e2f534b0e664c47
Ben Soutendijk <devaent1316@gmail.com>
2019-07-05 14:06:19 -0500

    remove old dashboard components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import axios from 'axios';
 
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/dashboard/Dashboard';
 import Header from './components/Header';
 import SplashPage from './components/SplashPage';
 import PageNotFound from './components/PageNotFound';
@@ -43,25 +42,6 @@ class App extends Component {
       moderators: [],
       chatters: [],
       chat: [],
-    }
-  }
-
-  async componentDidMount() {
-    const user = await axios.get('/api/auth/mixer/current');
-    if (user.status === 200) {
-      setTimeout(async () => this.setState({ user: user.data}));
-    }
-    const moderators = await axios.get(`https://mixer.com/api/v1/channels/${user.data.channelid}/users/mod`);
-    if (moderators.status === 200) {
-      setTimeout(async () => this.setState({ moderators: moderators.data}));
-    }
-    const chatters = await axios.get(`https://mixer.com/api/v2/chats/${user.data.channelid}/users`);
-    if (chatters.status === 200) {
-      setTimeout(async () => this.setState({ chatters: chatters.data}));
-    }
-    const chat = await axios.get(`/api/chat/mixer/${user.data.userid}`);
-    if (chat.status === 200) {
-      setTimeout(async () => this.setState({ chat: chat.data}));
     }
   }
 
