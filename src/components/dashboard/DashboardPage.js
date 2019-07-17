a0e8546bf5d9a8e3e67b9cd86f38dbfd164a01db
Ben Soutendijk <devaent1316@gmail.com>
2019-07-16 19:52:27 -0500

    get chat history from API and display on dashboard

--- src/components/dashboard/DashboardPage.js
+++ src/components/dashboard/DashboardPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios  from 'axios';
 
 import { makeStyles, createStyles } from '@material-ui/styles';
 
@@ -9,9 +10,43 @@ const useStyles = makeStyles((theme) => createStyles({
 function DashboardPage() {
   const classes = useStyles();
 
+  const [chat, setChat] = useState([]);
+
+  const renderChatMessage = (chatEvent) => {
+    let message = '';
+    const messageArray = chatEvent.data.message.message;
+    const username = chatEvent.data.user_name;
+    const roles = chatEvent.data.user_roles;
+    messageArray.forEach((chatMessage) => {
+      message = message + chatMessage.text;
+    });
+    return (
+      <div>
+        <span style={roles.includes('Mod') ? { color: 'green' } : null }>{username} </span>
+        <span>{message}</span>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    axios.get('/api/chat/mixer')
+      .then((res) => {
+        const { data } = res;
+        const chat = data.filter((chatEvent) => (
+          chatEvent.event === "ChatMessage"
+        ))
+        setChat(chat);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [])
+
   return (
     <div className={classes.root}>
-      <h1>Dashboard</h1>
+      {chat.map((chatEvent) => (
+        <p>{renderChatMessage(chatEvent)}</p>
+      ))}
     </div>
   )
 }
