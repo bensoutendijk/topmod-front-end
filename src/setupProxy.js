8d74e669f3e6ec2a5503f08b1830e833c09e89f7
Ben Soutendijk <devaent1316@gmail.com>
2019-07-20 00:35:50 -0500

    edit redirects

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'production') {
-    app.use(proxy('/api', { target: 'https://bgoodtv.herokuapp.com/' }));
+    app.use(proxy('/api', { target: 'https://topmod.herokuapp.com/' }));
   } else {
     app.use(proxy('/api', { target: 'http://localhost:5000/' }));
   }
